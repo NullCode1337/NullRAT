392ef75e57c655c99d02d9d54d0f2a6bd8996f8a
NullCode1337 <contact.nullcode@proton.me>
2022-01-25 00:24:45 +0600

    Update DepInst

--- dependencies/src/Main Operations/Exit.cs
+++ dependencies/src/Main Operations/Exit.cs
@@ -13,6 +13,17 @@
             }
             AnsiConsole.WriteLine();
             AnsiConsole.Write(new Rule());
+
+            if (ProgramData.PackagesFailed > 0)
+            {
+                AnsiConsole.MarkupLine("\n[maroon][[WARN]] Some packages have failed to be installed...[/]");
+
+                for (int i = 0; i < ProgramData.FailedPackages.Count; i++)
+                {
+                    AnsiConsole.MarkupLine($"Run: \"pip install {ProgramData.Packages[i]}\" to install the package manually");
+                }
+            }
+
             AnsiConsole.MarkupLine("\n[green][[INFO]] All Packages have been installed successfully![/]\n");
 
             Environment.Exit(0);
