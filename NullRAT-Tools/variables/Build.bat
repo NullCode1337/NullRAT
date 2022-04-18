83613e1ebff5fb503d2b1f5dbbdb8788300a0674
usrDottik <dotcomnose@gmail.com>
2022-04-18 15:05:21 -0300

    Fix some mistakes in batch comments.

--- NullRAT-Tools/variables/Build.bat
+++ NullRAT-Tools/variables/Build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 :: Publish NullRAT Compiler -> NATIVE
-:: for Windows x86 | Self Contained & Trimmed
+:: for Windows x64 | Self Contained & Trimmed
 dotnet publish "NullRAT Variables.csproj" --output build\ --framework net6.0 --arch x64 --os win -c release --self-contained true
