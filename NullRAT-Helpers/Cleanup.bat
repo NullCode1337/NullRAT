632cb8984a28aaaf0769c2cfe110d65da25eb49a
NullCode <contact.nullcode@proton.me>
2022-04-25 13:18:58 +0600

    Update cleanup for new structure

--- NullRAT-Helpers/Cleanup.bat
+++ NullRAT-Helpers/Cleanup.bat
@@ -1,16 +1,18 @@
 @echo off & color 8F
 attrib +h "%~nx0"
-mkdir "%~dp0\temp" & cd "%~dp0\src"
+mkdir "%~dp0\temp" & cd "%~dp0\NullRAT"
 move RAT.py "%~dp0\" 
 move custom_icon.ico "%~dp0\"
 move upx\upx.exe "%~dp0\"
-cd "%~dp0\" & rmdir /s /q "%~dp0\src\"
-mkdir "%~dp0\src\" & move RAT.py "%~dp0\src\"
-move custom_icon.ico "%~dp0\src\" & mkdir "%~dp0\src\upx\"
-move upx.exe "%~dp0\src\upx\" 
-move "2) NullRAT Compiler".exe "%~dp0\temp"
-move "1) NullRAT Dependencies".exe "%~dp0\temp"
+cd "%~dp0\" & rmdir /s /q "%~dp0\NullRAT\"
+mkdir "%~dp0\NullRAT\" & move RAT.py "%~dp0\NullRAT\"
+move custom_icon.ico "%~dp0\NullRAT\" & mkdir "%~dp0\NullRAT\upx\"
+move upx.exe "%~dp0\NullRAT\upx\" 
+move "3) NullRAT Compiler".bat "%~dp0\temp"
+move "2) NullRAT Variables".bat "%~dp0\temp"
+move "1) NullRAT Dependencies".bat "%~dp0\temp"
 if exist README.md (del README.md)
+if exist "Getting Variables".md (del README.md)
 if exist .git\ (rmdir /s /q .git\)
 del /f /q *.* 
 rmdir /s /q * & rmdir /s /q build\
