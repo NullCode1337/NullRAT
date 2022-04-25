9ab156e128ea8c2248b202b75ae66c5087988186
NullCode <ishaqahmed.dev@gmail.com>
2022-04-25 11:55:14 +0600

    Deleted all of dottik's tools as they are broken

--- NullRAT-Tools/dependencies/2) Build_IL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-:: Publish NullRAT Dependency Installer 
-:: for Windows x86 | Self Contained & Trimmed
-dotnet publish "Dependencies_IL.csproj" --output build\ -r win-x86 -c release --self-contained true
--- NullRAT-Tools/variables/Build_IL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-:: Publish NullRAT Compiler -> MSIL
-:: for Windows x86 | Self Contained & Trimmed
-dotnet publish "NullRAT Variables_IL.csproj" --output build\ --framework net6.0 --arch x86 --os win -c release --self-contained true
