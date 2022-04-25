dff8ee43f47e413a5831a510a1cb3b862c80808a
NullCode <contact.nullcode@proton.me>
2022-04-25 11:55:14 +0600

    Deleted all of dottik's tools as they are broken

--- NullRAT-Tools/dependencies/1) Restore.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Restore Project Dependencies, Useful when working with Visual Studio Code
-dotnet restore .\Dependencies.csproj
--- NullRAT-Tools/dependencies/2) Build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-:: Publish NullRAT Dependency Installer -> NATIVE
-:: for Windows x64 | Self Contained & Trimmed
-dotnet publish "Dependencies.csproj" --output build\ -r win-x64 -c release --self-contained true
