128e879f5b081e5aaca410077899e9a5e14799e5
NullCode1337 <ishaqahmed.dev@gmail.com>
2023-12-12 01:08:55 -0800

    Add package installer portion of new compiler

--- compiler.nim
+++ compiler.nim
@@ -1,5 +1,8 @@
 import std/terminal
 import std/os
+import std/browsers
+import std/osproc
+import std/[strutils, strtabs]
 
 proc cls() = 
     discard execShellCmd("cls")
@@ -65,8 +68,50 @@ proc printName() =
     stdout.styledWriteLine(fgRed, "  =========================================================")
     echo ""
 
+proc variablesCreator() = 
+	echo "vars"
+	
 proc packageInstaller() = 
-    echo "packageinstaller"
+    cls()
+    printName()
+    stdout.styledWriteLine({styleBright}, "  >> Dependencies Installer <<")
+    echo ""
+    stdout.styledWriteLine({styleBright}, "[1] Checking for Python...")
+    var status: int = execShellCmd("python --version")
+    if status == 0:
+        echo "Python installed!"
+		stdout.styledWriteLine({styleBright}, "[2] Checking if packages already installed...")
+		var result = execCmdEx("pip freeze")
+		var allInstalled: bool = true
+		if result.exitCode != 0:
+			echo "pip command failed to execute!!"
+		else:
+			if "pyinstaller==4.10" notin result or "virtualenv" notin result or "disnake" notin result or "requests" notin result or "pyarmor" notin result or "mss" notin result:
+				echo "Some dependencies are not installed"
+				allInstalled = false
+			if allInstalled:
+				echo "All packages installed and detected! Proceeding on with variables..."
+				sleep(3000)
+				variablesCreator()
+		
+	else:
+        echo "Python not installed!\nWould you like to download the recommended python installer? (Y/n): "
+        var input: char = getch();
+        if input == 'N' or input == 'n':
+            echo "NullRAT Builder cannot continue otherwise!!! Exiting..."
+            sleep(2000)
+            quit(1)
+        else:
+            openDefaultBrowser("https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe")
+            echo ""
+            echo "Your browser should start downloading the installer already."
+            echo "Since we do not support automatic installation of python, you have to run the installer manually"
+            echo "After running, please tick 'Install for All Users' and  'Add Python 3.8 to PATH', then install it"
+            echo "After installing, check if everything is functional by running NullRAT builder again."
+            echo ""
+            stdout.styledWriteLine({styleBright}, "Exiting in 30 seconds. Thank you for using NullRAT")
+            sleep(30000)
+            quit(2)
     
 proc mainMenu() =
     printName();
