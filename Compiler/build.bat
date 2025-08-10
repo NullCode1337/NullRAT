497cff5560bcc4fd213df008b9c40d796fe3351c
NullCode1337 <contact.nullcode@proton.me>
2025-08-10 22:17:06 +0600

    Compiler: Add instructions

--- Compiler/build.bat
+++ Compiler/build.bat
@@ -1,7 +1,11 @@
 @echo off
+rem Put their respective bin folders in PATH
+rem and %USERPROFILE%\.nimble\bin
+rem https://nim-lang.org/download/nim-2.2.4_x64.zip
+rem https://nim-lang.org/download/mingw64.7z
 
 cd %~dp0
 nimble install puppy
 nim c -d:release -d:danger main.nim
-move main.exe ..\compiler.exe
+move main.exe ..\Compiler.exe
 cd %~dp0..
